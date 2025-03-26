86c6ab504c60ca300f2fbf685c59afb5148ebf73
ARVIND KUMAR <127136310+hyarvind@users.noreply.github.com>
2025-03-26 15:23:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,11 @@ function App() {
       <img src={heroImage} alt="Description of the image" />
       <h1>Counter value: {counter}</h1>
       <p>Footer: {counter}</p>
+      <p>  As a B.Tech. Computer Science and Engineering student at MM(DU), I have developed a strong foundation in programming with proficiency in the MERN stack (MongoDB, Express.js, React, Node.js). Additionally, I possess expertise in web development technologies such as HTML, CSS, Bootstrap, JavaScript, and SQL, enabling me to build dynamic and responsive web applications.
+
+I am passionate about technology and its potential to streamline complex operations, enhance user experiences, and drive innovation. Outside of coding, I actively engage in exploring new learning opportunities to expand my skill set and stay updated with the latest technological trends.
+
+I am eager to contribute my technical expertise and continuously improve by tackling challenging projects and collaborating with like-minded individuals in the field. </p>
 
       <button onClick={addValue}>Add value {counter}</button>
       <br />
